45f49c21d332c7e017f8cb2410b4352f11075c97
Will Oliveira <williancontato67@gmail.com>
2023-10-19 22:07:54 -0300

    Update atualizarValorExcluido.js

--- assets/js/modules/atualizarValorExcluido.js
+++ assets/js/modules/atualizarValorExcluido.js
@@ -1,15 +1,14 @@
 export default function initAtualizarValorExcluido(item) {
     const itemConteiner = item.parentElement
-    const valorProduto = itemConteiner.querySelector(".precoProduto");
-    const quantidadeItens = itemConteiner.querySelector("input").value
-    const numeroTotalDeItens = document.querySelector(".totalItens")
-    const valorTotalCarrinho = document.querySelector(".valorTotal")
+    const valueProduct = itemConteiner.querySelector(".precoProduto").innerHTML.replace("R$", "").replace(".", "").replace(",", ".")
+    const qunatityItems = itemConteiner.querySelector("input").value
+    const totalNumberOfItems = document.querySelector(".totalItens")
+    const totalValueCart = document.querySelector(".valorTotal")
 
-    const valorFormat = +valorProduto.innerHTML.replace("R$", "").replace(".", "").replace(",", ".")
-    const valorTotalFormat = +valorTotalCarrinho.innerHTML.replace("R$", "")
-    const numeroTotalFormat = +numeroTotalDeItens.innerHTML.replace(" Produtos", "")
-    const valorFinal = valorTotalFormat - valorFormat
-    const numeroFinal = numeroTotalFormat - quantidadeItens
-    valorTotalCarrinho.innerHTML = `R$${valorFinal.toFixed(2)}`
-    numeroTotalDeItens.innerHTML = `${numeroFinal} Produtos`
+    const totalValueCartFormat = +totalValueCart.innerHTML.replace("R$", "")
+    const numeroTotalFormat = +totalNumberOfItems.innerHTML.replace(" Produtos", "")
+    const valorFinal = totalValueCartFormat - +valueProduct
+    const numeroFinal = numeroTotalFormat - qunatityItems
+    totalValueCart.innerHTML = `R$${valorFinal.toFixed(2)}`
+    totalNumberOfItems.innerHTML = `${numeroFinal} Produtos`
 }
